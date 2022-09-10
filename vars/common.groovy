03c9b632659e79de5b1b5bb3884486ab62ec7f55
Divyanarina <divyanarina511@gmail.com>
2022-09-10 19:54:11 +0530

    Shared library

--- vars/common.groovy
+++ vars/common.groovy
@@ -4,3 +4,11 @@ def pipelineInit() {
         git branch: 'main', url: "https://github.com/Satish439/${COMPONENT}.git"
     }
 }
+
+def publishArtifacts() {
+    stage('publish Artifacts if tags are there') {
+        if (env.APP_TYPE == "nodejs") {
+            sh 'zip -r ${COMPONENT}-{TAG_NAME}.zip node_modules server.js'
+        }
+    }
+}
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -3,11 +3,20 @@ def call() {
 
         common.pipelineInit()
 
-    stage('Download Dependencies') {
-        sh '''
+        stage('Download Dependencies') {
+            sh '''
                ls -l
                npm install 
              '''
+        }
     }
-}
+
+
+//if both are equal then it is definitely a tag//
+
+    if (env.BRANCH_NAME == env.TAG_NAME)
+
+    {
+        common.publishArtifacts()
+    }
 }
