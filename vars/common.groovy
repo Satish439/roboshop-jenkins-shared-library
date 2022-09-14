fac478e506b0e3cc7599b434e27e96e5f94dd685
Divyanarina <divyanarina511@gmail.com>
2022-09-14 06:55:58 +0530

    Shared library

--- vars/common.groovy
+++ vars/common.groovy
@@ -6,16 +6,25 @@ def pipelineInit() {
 }
 
 def publishArtifacts() {
-    stage('publish Artifacts if tags are there') {
+    stage('Prepare Artifacts if tags are there') {
         if (env.APP_TYPE == "nodejs") {
             sh 'zip -r ${COMPONENT}-{TAG_NAME}.zip node_modules server.js'
         }
+
+        if (env.APP_TYPE == 'maven') {
+
+            sh 'mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar'
+            sh 'zip -r ${COMPONENT}-{TAG-NAME}.zip ${COMPONENT}.jar'
+
+        }
     }
 
-    stage('Push Artifacts to Nexus') {
-        withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'password', usernameVariable: 'user')]) {
-            sh 'curl -v -u --user <"${USER}":"${PASSWORD}">  --upload-file ${COMPONENT}-${TAG_NAME}.zip http://34.238.85.190:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip'
 
+    stage('Push Artifacts to Nexus') {
+        withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'pass', usernameVariable: 'user')]) {
+            sh """
+        curl -v -u ${user}:${pass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.7.89:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+      """
         }
     }
 }
--- vars/maven.groovy
+++ vars/maven.groovy
@@ -2,10 +2,14 @@ def call() {
     node() {
 
         common.pipelineInit()
-      stage ('Compile and build code') {
-          sh 'sudo yum install maven -y'
-          sh 'mvn clean package'
+        stage('Compile and build code') {
+            sh 'sudo yum install maven -y'
+            sh 'mvn clean package'
 
-      }
+        }
+        if (env.BRANCH_NAME == env.TAG_NAME)
+        {
+            common.publishArtifacts()
+        }
     }
 }
