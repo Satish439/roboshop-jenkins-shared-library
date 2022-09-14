f1eddc299db3b291761f70b64412bd762c1e6baa
Divyanarina <divyanarina511@gmail.com>
2022-09-14 07:02:58 +0530

    Shared library

--- vars/common.groovy
+++ vars/common.groovy
@@ -23,7 +23,7 @@ def publishArtifacts() {
     stage('Push Artifacts to Nexus') {
         withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'pass', usernameVariable: 'user')]) {
             sh """
-        curl -v -u ${user}:${pass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://34.201.251.207:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+        curl -u ${user}:${pass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://34.201.251.207:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
       """
         }
     }
