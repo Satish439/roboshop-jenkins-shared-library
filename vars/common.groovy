f5dae764b4cf01b332b12d91114740e0bb428d0f
Divyanarina <divyanarina511@gmail.com>
2022-09-19 06:52:47 +0530

    Shared library

--- vars/common.groovy
+++ vars/common.groovy
@@ -23,51 +23,48 @@ def publishArtifacts() {
     stage('Push Artifacts to Nexus') {
         withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'password', usernameVariable: 'username')]) {
             sh """
-        curl -u ${USERNAME}:${PASSWORD} --upload-file ${ENV}-${COMPONENT}-${TAG_NAME}.zip http://3.91.226.208:8081/repository/${COMPONENT}/${ENV}-${COMPONENT}-${TAG_NAME}.zip
+        curl -u ${USERNAME}:${PASSWORD} --upload-file ${ENV}-${COMPONENT}-${TAG_NAME}.zip http://44.204.0.216:8081/repository/${COMPONENT}/${ENV}-${COMPONENT}-${TAG_NAME}.zip
       """
         }
     }
-    stage('Deploy to Dev Env') {
-        build job: 'deploy-to-any-env', parameters: [string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'ENV', value: "${ENV}"), string(name: 'APP_VERSION', value: "${TAG_NAME}")]
-    }
-
-    stage('Run Smoke Tests on DEV') {
-        sh "echo Smoke tests"
-    }
-    promoteRelease("dev", "qa")
-
-    stage('Deploy to QA Env') {
-        //build job: 'deploy-to-any-env', parameters: [string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'ENV', value: "qa"), string(name: 'APP_VERSION', value: "${TAG_NAME}")]
-        echo 'QA Deploy'
-    }
-
-    testRuns()
-
-    stage('Run Smoke Tests on QA') {
-        sh "echo Smoke tests"
-    }
-}
-
-
+//    stage('Deploy to Dev Env') {
+//        build job: 'deploy-to-any-env', parameters: [string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'ENV', value: "${ENV}"), string(name: 'APP_VERSION', value: "${TAG_NAME}")]
+//    }
+//
+//    stage('Run Smoke Tests on DEV') {
+//        sh "echo Smoke tests"
+//    }
+//    promoteRelease("dev", "qa")
+//
+//    stage('Deploy to QA Env') {
+//        //build job: 'deploy-to-any-env', parameters: [string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'ENV', value: "qa"), string(name: 'APP_VERSION', value: "${TAG_NAME}")]
+//        echo 'QA Deploy'
+//    }
+//
+//    testRuns()
+//
+//    stage('Run Smoke Tests on QA') {
+//        sh "echo Smoke tests"
+//    }
+//}
 
 
+    def codeChecks() {
+        stage('Quality Checks & Unit Tests') {
+            parallel([
+                    qualityChecks: {
+                        withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'pass', usernameVariable: 'user')]) {
+                            sh "sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.host.url=http://44.204.0.216:9000 -Dsonar.login=${user} -Dsonar.password=${pass}"
+                            sh 'sonar-quality-gate.sh $(user) ${pass} 52.201.103.29 ${COMPONENT} '
 
-def codeChecks() {
-    stage('Quality Checks & Unit Tests') {
-        parallel([
-                qualityChecks: {
-                    withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'pass', usernameVariable: 'user')]) {
-                        sh "sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.host.url=http://52.201.103.29:9000 -Dsonar.login=${user} -Dsonar.password=${pass}"
-                        sh 'sonar-quality-gate.sh $(user) ${pass} 52.201.103.29 ${COMPONENT} '
-
+                        }
+                    },
+                    unitTests    : {
+                        unitTests()
                     }
-                },
-                unitTests: {
-                    unitTests()
-                }
-        ])
+            ])
+        }
     }
-}
 
     def unitTests() {
         if (env.APP_TYPE == "nodejs") {
@@ -78,3 +75,4 @@ def codeChecks() {
 
         }
     }
+}
